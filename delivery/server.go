a9ed4326a068f44dcbd11c884d5b59659dc6aa3b
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-08-22 21:27:23 +0700

    add: server.go

---
+++ delivery/server.go
@@ -0,0 +1,68 @@
+package delivery
+
+import (
+	"fmt"
+
+	"github.com/NursiNursi/laundry-apps/config"
+	"github.com/NursiNursi/laundry-apps/delivery/controller/api"
+	"github.com/NursiNursi/laundry-apps/repository"
+	"github.com/NursiNursi/laundry-apps/usecase"
+	"github.com/NursiNursi/laundry-apps/utils/exceptions"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	// semua usecase di taruh disini (interface)
+	uomUC      usecase.UomUseCase
+	productUC  usecase.ProductUseCase
+	customerUC usecase.CustomerUseCase
+	employeeUC usecase.EmployeeUseCase
+	billUC     usecase.BillUseCase
+	engine     *gin.Engine
+	host       string
+}
+
+func (s *Server) Run() {
+	s.initController()
+	err := s.engine.Run(s.host)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) initController() {
+	// semua controller disini
+	api.NewUomController(s.uomUC, s.engine)
+	api.NewProductController(s.engine, s.productUC)
+	api.NewCustomerController(s.engine, s.customerUC)
+	api.NewEmployeeController(s.engine, s.employeeUC)
+	api.NewBillController(s.engine, s.billUC)
+}
+
+func NewServer() *Server {
+	cfg, err := config.NewConfig()
+	exceptions.CheckErr(err)
+	dbConn, _ := config.NewDbConnection(cfg)
+	db := dbConn.Conn()
+	uomRepo := repository.NewUomRepository(db)
+	productRepo := repository.NewProductRepository(db)
+	customerRepo := repository.NewCustomerRepository(db)
+	employeeRepo := repository.NewEmployeeRepository(db)
+	billRepo := repository.NewBillRepository(db)
+	uomUseCase := usecase.NewUomUseCase(uomRepo)
+	productUseCase := usecase.NewProductUseCase(productRepo, uomUseCase)
+	customerUseCase := usecase.NewCustomerUseCase(customerRepo)
+	employeeUseCase := usecase.NewEmployeeUseCase(employeeRepo)
+	billUseCase := usecase.NewBillUseCase(billRepo, employeeUseCase, customerUseCase, productUseCase)
+	engine := gin.Default()
+	host := fmt.Sprintf("%s:%s", cfg.ApiHost, cfg.ApiPort)
+	return &Server{
+		uomUC:      uomUseCase,
+		productUC:  productUseCase,
+		customerUC: customerUseCase,
+		employeeUC: employeeUseCase,
+		billUC:     billUseCase,
+		engine:     engine,
+		host:       host,
+	}
+}
