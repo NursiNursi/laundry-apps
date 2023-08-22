5e55a3c372b1302b49fefdde16b1dc65ee525217
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-08-22 21:27:04 +0700

    modify: uom model and repository

--- repository/uom_repository.go
+++ repository/uom_repository.go
@@ -3,12 +3,11 @@ package repository
 import (
 	"database/sql"
 
-	"fmt"
-
 	"github.com/NursiNursi/laundry-apps/model"
-	"github.com/NursiNursi/laundry-apps/utils/exceptions"
 )
 
+// Karena dia sebuah interface, maka wajib kita implementasikan semuanya
+// mulai dari Create s.d Delete
 type UomRepository interface {
 	BaseRepository[model.Uom]
 	GetByName(name string) (model.Uom, error)
@@ -18,54 +17,70 @@ type uomRepository struct {
 	db *sql.DB
 }
 
+// Method -> ada sebuah receiver ((u *uomRepository))
 func (u *uomRepository) Create(payload model.Uom) error {
 	_, err := u.db.Exec("INSERT INTO uom (id, name) VALUES ($1, $2)", payload.Id, payload.Name)
-	exceptions.CheckErr(err)
-	fmt.Println("UOM created successfully")
+	if err != nil {
+		return err
+	}
 	return nil
 }
+
 func (u *uomRepository) List() ([]model.Uom, error) {
 	rows, err := u.db.Query("SELECT id, name FROM uom")
-	exceptions.CheckErr(err)
+	if err != nil {
+		return nil, err
+	}
 
 	var uoms []model.Uom
 	for rows.Next() {
 		var uom model.Uom
 		err := rows.Scan(&uom.Id, &uom.Name)
-		exceptions.CheckErr(err)
+		if err != nil {
+			return nil, err
+		}
 		uoms = append(uoms, uom)
 	}
-	fmt.Println("UOM successfully retreived")
 	return uoms, nil
 }
 
 func (u *uomRepository) Get(id string) (model.Uom, error) {
 	var uom model.Uom
 	err := u.db.QueryRow("SELECT id, name FROM uom WHERE id=$1", id).Scan(&uom.Id, &uom.Name)
-	exceptions.CheckErr(err)
+	if err != nil {
+		return model.Uom{}, err
+	}
 	return uom, nil
 }
 
 func (u *uomRepository) GetByName(name string) (model.Uom, error) {
 	var uom model.Uom
-	// LIKE => case sensitive ILIKE => incase sensitive ()
+	// LIKE => case sensitive e.g L l (ngaruh)
+	// ILIKE => in case sensitibe e.g L l (tidak ngaruh) (hanya ada di postgre)
 	err := u.db.QueryRow("SELECT id, name FROM uom WHERE name ILIKE $1", "%"+name+"%").Scan(&uom.Id, &uom.Name)
-	exceptions.CheckErr(err)
+	if err != nil {
+		return model.Uom{}, err
+	}
 	return uom, nil
 }
 
 func (u *uomRepository) Update(payload model.Uom) error {
 	_, err := u.db.Exec("UPDATE uom SET name=$1 WHERE id=$2", payload.Name, payload.Id)
-	exceptions.CheckErr(err)
-	return nil
-}
-func (u *uomRepository) Delete(id string) error {
-	_, err := u.db.Exec("DELETE FROM uom WHERE id=$1", id)
-	exceptions.CheckErr(err)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// constructor
+func (u *uomRepository) Delete(id string) error {
+	_, err := u.db.Exec("DELETE FROM uom WHERE id=$1", id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Constructor
 func NewUomRepository(db *sql.DB) UomRepository {
 	return &uomRepository{db: db}
 }
